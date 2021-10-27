a1e2eda649c050e6f3b4aaa8c48f4d2197761f75
dadiborn <36768799+dadiborn@users.noreply.github.com>
2021-10-27 20:56:26 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import {data/*<< Cannot resolve symbol 'data' */} from './comp/data';
-	//import {data/*<< Cannot resolve symbol 'data' */} from './comp/data.ts';  /* << adding .ts solves issue*/
+	//import {data} from './comp/data.ts';  /* << adding .ts solves issue*/
 	import Data from './comp/Data.svelte';
 
 	console.log(data.a)
