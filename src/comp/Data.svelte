674030af572e55f5f553070fcd7bcb2886cc7109
dadiborn <36768799+dadiborn@users.noreply.github.com>
2021-10-27 20:52:00 +0300

    Add files via upload

---
+++ src/comp/Data.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+import {data/*<< Cannot resolve symbol 'data' */} from './data';
+//import {data} from './data.ts'; /* << adding .ts solves issue*/
+console.log(data.a)
+
+</script>
+<h1>hello from data</h1>
